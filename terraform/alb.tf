e43bf29c54750278ce2313ba88541d5ff670d3dd
serebryakovmv <serebryakovmv@sovcombank.ru>
2023-01-13 16:57:28 +0400

    add backend for alb

---
+++ terraform/alb.tf
@@ -0,0 +1,24 @@
+resource "yandex_alb_backend_group" "this" {
+  name = "alb-backend"
+  
+  http_backend {
+    name                   = "http-backend"
+    weight                 = 1
+    port                   = 80
+    target_group_ids       = [
+      yandex_alb_target_group.nginx-target-group.id
+    ]
+    load_balancing_config {
+      panic_threshold      = 90
+    }    
+    healthcheck {
+      timeout              = "10s"
+      interval             = "2s"
+      healthy_threshold    = 10
+      unhealthy_threshold  = 15 
+      http_healthcheck {
+        path               = "/"
+      }
+    }
+  }
+}
