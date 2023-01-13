84ab65131e6442ddf0882b528fa9d383e00a8f90
serebryakovmv <serebryakovmv@sovcombank.ru>
2023-01-13 10:51:42 +0400

    fix subnets ids

--- terraform/instances.tf
+++ terraform/instances.tf
@@ -18,7 +18,7 @@ resource "yandex_compute_instance_group" "this" {
     }
     network_interface {
       network_id = yandex_vpc_network.this.id
-      subnet_ids = ["${yandex_vpc_subnet.this.id}"]
+      subnet_ids = ["${yandex_vpc_subnet.this[each.key].id}"]
     }
     metadata = {
       ssh-keys = "yc-user:${file("~/.ssh/id_rsa.pub")}"
