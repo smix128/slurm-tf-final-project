fe959db992c8638538c7a193fa8d63661c02ddd6
smix128 <smix128@gmail.com>
2023-01-14 13:24:06 +0400

    add output real balancer ip

--- terraform/alb.tf
+++ terraform/alb.tf
@@ -70,5 +70,5 @@ resource "yandex_alb_load_balancer" "this" {
 }
 
 output "real-balancer-ip" {
-  value = tolist(tolist(tolist(tolist(yandex_lb_network_load_balancer.this.listener).0.endpoint).0.address).0.external_ipv4_address).0.address
+  value = tolist(tolist(tolist(tolist(yandex_alb_load_balancer.this.listener).0.endpoint).0.address).0.external_ipv4_address).0.address
 }
